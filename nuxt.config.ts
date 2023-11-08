205cdd682e09852bf354ab6be670745b99190d58
Konstantinos Maninakis <maninak@protonmail.com>
2023-11-08 16:20:14 +0100

    chore(deps): add tailwindcss support
    Also refactor to enable vue-router-powered routing
    
    Signed-off-by: Konstantinos Maninakis <maninak@protonmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
   devtools: { enabled: true },
 })
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="text-blue-800">Testing tailwind (delete me)</div>
+  <NuxtWelcome/>
+</template>
