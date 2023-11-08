f866e56b9ae5888b681ae1f391f7841fca65fc9f
Konstantinos Maninakis <maninak@protonmail.com>
2023-11-08 21:12:13 +0100

    chore(deps): add nuxt-icon support
    Makes it possible to easily use any icon from
    the https://icones.js.org/ collection
    
    Signed-off-by: Konstantinos Maninakis <maninak@protonmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@vueuse/nuxt', 'nuxt-icon'],
   devtools: { enabled: true },
 })
