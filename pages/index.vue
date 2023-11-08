1a98486e0aecba8de7862e15ad9eb8a1bdd7055d
Konstantinos Maninakis <maninak@protonmail.com>
2023-11-08 21:19:11 +0100

    fix(tailwind): enable dark mode across whole app, not only for root page
    Signed-off-by: Konstantinos Maninakis <maninak@protonmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts">
-  /*
-   * Sets class `dark` on `html` elem (if browser `prefers-color-scheme: dark`)
-   * which activates TailwindCSS class-based dark mode
-   */
-  useDark()
-</script>
-
 <template>
   <div class="bg-rad-fill-ghost text-rad-foreground-contrast">
     Testing tailwind, rad themed colors and dark-mode support (delete me)
