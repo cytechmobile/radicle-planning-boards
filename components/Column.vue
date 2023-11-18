127ff1294de63de93b1021c0fb202f2d9a324eb7
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2023-11-18 13:22:12 +0200

    feat: implement Column and IssueCard components
    Implement Column and IssueCard components
    Introduce /board page and style it to accommodate the components
    Display mock data on the page.
    
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

---
+++ components/Column.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const { title } = defineProps<{ title: string }>()
+</script>
+
+<template>
+  <div class="border-rad-border-hint border flex-1 max-w-[350px] min-w-[350px] rounded-[4px]">
+    <h3 class="p-2 font-medium text-lg capitalize">{{ title }}</h3>
+
+    <ul class="p-2 flex flex-col gap-2">
+      <slot />
+    </ul>
+  </div>
+</template>
---
+++ components/IssueCard.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+// eslint-disable-next-line prettier/prettier
+const { title } = defineProps<{ id: string, title: string }>()
+</script>
+
+<template>
+  <li
+    class="bg-rad-background-float border-rad-border-hint border rounded-[4px] pt-2 px-2 pb-6 hover:bg-rad-fill-float-hover flex flex-col gap-1"
+  >
+    <span class="text-xs text-rad-foreground-emphasized font-medium">
+      {{ id.slice(0, 7) }}
+    </span>
+    <a href="#" class="hover:underline w-fit text-sm">{{ title }}</a>
+  </li>
+</template>
