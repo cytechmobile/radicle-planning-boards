5914c76e3c87a6b6c670b2842483045e968f6451
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2023-11-28 20:41:22 +0200

    feat(column): add icon and issue counter to column header
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

--- components/Column.vue
+++ components/Column.vue
@@ -4,13 +4,30 @@ import type { Issue, IssueStatus } from '../constants/issues'
 
 const props = defineProps<{ title: IssueStatus; issues: Issue[] }>()
 const issuesModel = ref(unref(toRefs(props).issues)) // create new ref with the same contents
+
+const TITLE_TO_ICON_MAP = {
+  todo: 'heroicons:clipboard-document-list',
+  doing: 'heroicons:cog-6-tooth',
+  done: 'heroicons:check-badge',
+} satisfies Record<IssueStatus, string>
 </script>
 
 <template>
   <div
     class="flex min-w-[350px] max-w-[350px] flex-1 flex-col rounded border border-rad-border-hint bg-rad-background-dip"
   >
-    <h3 class="p-2 text-xl font-semibold capitalize">{{ title }}</h3>
+    <div class="flex items-center justify-between p-2">
+      <div class="flex items-center gap-2">
+        <Icon :name="TITLE_TO_ICON_MAP[title]" size="24" />
+        <h3 class="text-xl font-semibold capitalize">{{ title }}</h3>
+      </div>
+
+      <small
+        class="rounded bg-rad-fill-ghost px-3 py-0.5 text-sm font-semibold text-rad-foreground-gray"
+      >
+        {{ issuesModel.length }} {{ issuesModel.length === 1 ? 'issue' : 'issues' }}
+      </small>
+    </div>
 
     <VueDraggable
       v-model="issuesModel"
