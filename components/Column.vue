991a0b6de1bbcdf2da2cfe75720ffb9d886b28a3
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2023-12-03 09:17:18 +0200

    feat(column): add status icons and card counter on columns
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

--- components/Column.vue
+++ components/Column.vue
@@ -4,13 +4,30 @@ import type { Issue, IssueStatus } from '../constants/issues'
 
 const props = defineProps<{ title: IssueStatus; issues: Issue[] }>()
 const issuesModel = ref(unref(toRefs(props).issues)) // create new ref with the same contents
+
+const STATUS_TO_ICON_MAP = {
+  todo: { name: 'bx:circle', class: 'text-rad-foreground-contrast' },
+  doing: { name: 'bx:adjust', class: 'text-rad-foreground-yellow' },
+  done: { name: 'bx:bxs-circle', class: 'text-rad-foreground-success' },
+} satisfies Record<IssueStatus, { name: string; class: string }>
 </script>
 
 <template>
   <div
     class="flex min-w-[350px] max-w-[350px] flex-1 flex-col rounded border border-rad-border-hint bg-rad-background-dip"
   >
-    <h3 class="px-5 py-2 text-xl font-semibold capitalize">{{ title }}</h3>
+    <div class="flex items-baseline gap-2 p-2">
+      <Icon
+        :name="STATUS_TO_ICON_MAP[title].name"
+        size="20"
+        :class="`translate-y-1 ${STATUS_TO_ICON_MAP[title].class}`"
+      />
+      <h3 class="font-semibold capitalize">{{ title }}</h3>
+
+      <small class="text-sm font-semibold text-rad-foreground-gray">
+        {{ issuesModel.length }}
+      </small>
+    </div>
 
     <VueDraggable
       v-model="issuesModel"
