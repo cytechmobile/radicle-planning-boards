3019da2902c21c5f46bfa9891e3d6b4c43c7b874
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2023-12-10 14:14:33 +0200

    feat(iframe): update theme on radicle-interface message event
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+type Theme = 'light' | 'dark'
+
+interface Message {
+  type: 'theme'
+  theme: Theme
+}
+
+function setTheme(theme: Theme) {
+  document.documentElement.classList.remove('dark')
+  document.documentElement.classList.remove('light')
+  document.documentElement.classList.add(theme)
+}
+
+onMounted(() => {
+  window.addEventListener('message', (event: MessageEvent<Message>) => {
+    switch (event.data.type) {
+      case 'theme':
+        setTheme(event.data.theme)
+        break
+      default:
+    }
+  })
+})
+</script>
+
 <template>
   <main class="flex min-h-screen flex-col">
     <slot></slot>
