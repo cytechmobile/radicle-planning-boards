8063c5768a25e0a723996f456b4e074e5cddae9b
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2023-12-12 20:35:31 +0200

    refactor(theme): extract theme logic into useTheme composable
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,5 @@
 <script setup lang="ts">
-type Theme = 'light' | 'dark'
-
-interface Message {
-  type: 'theme'
-  theme: Theme
-}
-
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
-
-function setTheme(theme: Theme) {
-  toggleDark(theme === 'dark')
-}
-
-const { initialTheme } = useRoute().query
-
-onMounted(() => {
-  if (initialTheme === 'light' || initialTheme === 'dark') {
-    setTheme(initialTheme)
-  }
-})
-
-useEventListener(window, 'message', (event: MessageEvent<Message>) => {
-  if (event.data.type === 'theme') {
-    setTheme(event.data.theme)
-  }
-})
+useTheme()
 </script>
 
 <template>
