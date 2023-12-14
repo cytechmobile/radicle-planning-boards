60f75201ece6a9c52723e1c084fa46e24a4bbcdc
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2023-12-14 08:19:31 +0200

    refactor(routes): use routeRules instead of middleware for redirecting
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

--- middleware/redirect.global.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware(
-  // eslint `consistent-return` conflicts with `no-useless-return` here
-  // eslint-disable-next-line consistent-return
-  async (to) => {
-    if (!to.path.match(/^\/[^\/]+\/[^\/]+/)) {
-      return await navigateTo('/seed.radicle.xyz:443/rad:z4V1sjrXqjvFdnCUbxPFqd5p4DtH5')
-    }
-  },
-)
