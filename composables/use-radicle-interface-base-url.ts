4cb9aaf3c8d91f3ad50dc93291767f5269e98398
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2023-12-14 20:45:06 +0200

    feat(cards): accept radicle-interface baseUrl query param
    If provided in the url, use the "baseUrl" query param to create the href for the card links
    
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

--- composables/use-radicle-interface-base-url.ts
+++ composables/use-radicle-interface-base-url.ts
@@ -3,12 +3,18 @@ import { useStorage } from '@vueuse/core'
 export function useRadicleInterfaceBaseUrl(): string {
   const defaultRadicleInterfaceBaseUrl = 'https://app.radicle.xyz'
 
+  const { baseUrl } = useRoute().query
+
   const configuredRadicleInterfaceBaseUrl = useStorage(
     'RPB_config-radicle-interface-base-url',
     defaultRadicleInterfaceBaseUrl,
   )
 
   try {
+    if (typeof baseUrl === 'string') {
+      return new URL(baseUrl).origin
+    }
+
     return new URL(configuredRadicleInterfaceBaseUrl.value).origin
   } catch {
     return defaultRadicleInterfaceBaseUrl
