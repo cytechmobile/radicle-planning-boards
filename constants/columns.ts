0a80aa345fb236b16dcc39998135c9d2b399c9e1
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2023-12-28 20:49:13 +0200

    feat(board): add support for deleting columns
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

--- constants/columns.ts
+++ constants/columns.ts
@@ -1,1 +1,5 @@
+// These columns are required to be present in the board.
+// non-planned is the default column for issues without a label, and done is the default column for closed issues.
+export const requiredColumns = ['non-planned', 'done']
+
 export const initialColumns = ['non-planned', 'todo', 'doing', 'done']
--- stores/board.ts
+++ stores/board.ts
@@ -7,9 +7,17 @@ export const useBoardStore = defineStore('board', () => {
     columns.value = [...new Set([...columns.value, ...parsedColumns])]
   }
 
+  function removeColumn(column: string) {
+    const columnIndex = columns.value.indexOf(column)
+    if (columnIndex !== -1) {
+      columns.value.splice(columnIndex, 1)
+    }
+  }
+
   const store = {
     columns,
     mergeColumns,
+    removeColumn,
   }
 
   return store
