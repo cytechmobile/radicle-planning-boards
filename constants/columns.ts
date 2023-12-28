c3e3137a6e95a00605ddab437bf6ed8ad494d163
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2023-12-28 19:06:10 +0200

    refactor(board): remove Column type in favor of dynamic strings
    Refactor board logic to allow for additional dynamic columns based on issues labels on top of
    initial columns
    
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

--- constants/columns.ts
+++ constants/columns.ts
@@ -1,9 +1,1 @@
-import z from 'zod'
-
-export const columns = ['non-planned', 'todo', 'doing', 'done'] as const
-
-export const columnSchema = z.enum(columns)
-
-export type Column = z.infer<typeof columnSchema>
-
-export type PlannedColumn = Exclude<Column, 'non-planned'>
+export const initialColumns = ['non-planned', 'todo', 'doing', 'done'] as const
