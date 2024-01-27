1ba391182715fea0989582ce3914a4ae6128b8f5
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2024-01-27 11:07:22 +0200

    feat(board): allow re-ordering columns
    Implement column re-ordering (not persisted)
    
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

--- pages/[node]/[rid].vue
+++ pages/[node]/[rid].vue
@@ -1,20 +1,38 @@
 <script setup lang="ts">
+import { VueDraggable } from 'vue-draggable-plus'
+
 const auth = useAuthStore()
 const issues = useIssuesStore()
+
+const columnsModel = ref<string[]>([])
 
 const columns = computed(() =>
   issues.issuesByColumn ? Object.keys(issues.issuesByColumn) : null,
 )
+
+watchEffect(() => {
+  if (columns.value) {
+    columnsModel.value = [...columns.value]
+  }
+})
 </script>
 
 <template>
   <div v-if="columns" class="flex flex-1 gap-4 overflow-x-auto px-4 pb-4">
-    <Column
-      v-for="column in columns"
-      :key="column"
-      :title="column"
-      :issues="issues.issuesByColumn?.[column] ?? []"
-    />
+    <VueDraggable
+      v-model="columnsModel"
+      class="flex gap-4"
+      ghost-class="opacity-50"
+      group="columns"
+      :animation="150"
+    >
+      <Column
+        v-for="column in columns"
+        :key="column"
+        :title="column"
+        :issues="issues.issuesByColumn?.[column] ?? []"
+      />
+    </VueDraggable>
     <NewColumn v-if="auth.isAuthenticated" />
   </div>
 </template>
