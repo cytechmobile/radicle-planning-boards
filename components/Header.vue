0b93779afc90f1de148acc5fdb10032a84ae2dd7
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2024-03-24 19:03:01 +0200

    refactor(header): extract button styles into class
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

--- components/Header.vue
+++ components/Header.vue
@@ -46,21 +46,13 @@ async function handleImport() {
     <div class="flex gap-4">
       <template v-if="auth.isAuthenticated">
         <UTooltip text="Copy board state to clipboard">
-          <button
-            class="flex h-8 cursor-pointer items-center justify-center gap-2 rounded-sm bg-rad-fill-secondary px-3 text-sm font-semibold text-rad-foreground-match-background hover:bg-rad-fill-secondary-hover"
-            type="button"
-            @click="handleExport"
-          >
+          <button class="button-solid" type="button" @click="handleExport">
             <Icon :name="isExportSuccess ? 'octicon:check-16' : 'octicon:copy-16'" size="16" />
             Export
           </button>
         </UTooltip>
         <UTooltip text="Import board state from clipboard">
-          <button
-            class="flex h-8 cursor-pointer items-center justify-center gap-2 rounded-sm bg-rad-fill-secondary px-3 text-sm font-semibold text-rad-foreground-match-background hover:bg-rad-fill-secondary-hover"
-            type="button"
-            @click="handleImport"
-          >
+          <button class="button-solid" type="button" @click="handleImport">
             <Icon
               :name="isImportSuccess ? 'octicon:check-16' : 'octicon:arrow-down-16'"
               size="16"
