38831a51985a550e1bd18e47de58fbc63f49e20d
Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>
2024-06-16 15:21:39 +0300

    revert(lint): revert eslint changes
    Signed-off-by: Zacharias Fragkiadakis <zacfragkiadakis@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,8 @@
 import maninak from '@maninak/eslint-config'
 
-export default maninak(
-  {
-    ignores: ['*.md'],
-    typescript: {
-      tsconfigPath: 'tsconfig.json',
-    },
+export default maninak({
+  ignores: ['*.md'],
+  typescript: {
+    tsconfigPath: 'tsconfig.json',
   },
-  {
-    files: ['**/*.test.ts'],
-    rules: {
-      'max-len': ['warn', { code: 95, ignoreStrings: true }],
-    },
-  },
-)
+})
